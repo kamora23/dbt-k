4a40a017abdd9c59091267bdea27fc5a6d8e9fc2
John Müller <a.tubler1@outlook.com>
2023-11-22 06:52:09 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select
+    -- from raw orders
+    ord.orderid,
+    ord.orderdate,
+    ord.shipdate,
+    ord.shipmode,
+    ord.ordercostprice,
+    ord.ordersellingprice,
+    ord.ordersellingprice - ord.ordercostprice as orderprofit,
+    -- from raw_customer
+    cus.customername,
+    cus.segment,
+    cus.country,
+    -- from raw_product
+    pro.category,
+    pro.productname,
+    pro.subcategory
+from {{ ref('raw_orders') }} as ord
+left join {{ ref('raw_customer') }} as cus
+on ord.customerid = cus.customerid
+left join {{ ref('raw_product') }} as pro
+on ord.productid = pro.productid
